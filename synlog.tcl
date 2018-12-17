c2edc7e2834febe9ee6248de83058ec7aafb826c
Ben Bylsma <bylsma.1@osu.edu>
2018-12-17 14:37:02 -0500

    Version a.0b  Moved test_points.v from dcfeb3a\source to dcfeb3a_vttx\source and recompiled (no code changed).  Stopped tracking stdout.log.bak.*.

--- synlog.tcl
+++ synlog.tcl
@@ -1,2 +1,4 @@
-project -load dcfeb3a_vttx.prj
-run_tcl -fg dcfeb3a_vttx_map.tcl
+history clear
+project -load F:/DCFEB/firmware/ISE_14.7/dcfeb3a_vttx/dcfeb3a_vttx.prj
+project -load F:/DCFEB/firmware/ISE_14.7/dcfeb3a_vttx/dcfeb3a_vttx.prj
+project -close F:/DCFEB/firmware/ISE_14.7/dcfeb3a_vttx/dcfeb3a_vttx.prj
